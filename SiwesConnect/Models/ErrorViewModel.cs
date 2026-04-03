6b0cd7b54a3c05a6bc68a2ad2de241c95246f2cd
Twik24 <israeltaiwo86@gmail.com>
2026-04-03 12:06:29 +0100

    Add project files.

---
+++ SiwesConnect/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SiwesConnect.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
